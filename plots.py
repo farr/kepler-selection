08621f50854699c2356bc2f9de7113fe99271072
Will Meierjurgen Farr <wmfarr@gmail.com>
2014-04-16 15:48:26 +0100

    Some of plots.py adapted to new parameterisation.

--- plots.py
+++ plots.py
@@ -28,10 +28,10 @@ def plot_error_ellipse_and_bounds(logpost, p):
     p = logpost.to_params(p)
 
     plot_error_ellipse(logpost, p)
-    pp.axvline(np.exp(p['lower_left'][0]), color='k')
-    pp.axvline(np.exp(p['upper_right'][0]), color='k')
-    pp.axhline(np.exp(p['lower_left'][1]), color='k')
-    pp.axhline(np.exp(p['upper_right'][1]), color='k')
+    pp.axvline(np.exp(logpost.lower_left[0]), color='k')
+    pp.axvline(np.exp(logpost.upper_right[0]), color='k')
+    pp.axhline(np.exp(logpost.lower_left[1]), color='k')
+    pp.axhline(np.exp(logpost.upper_right[1]), color='k')
 
 def plot_true_foreground_density(logpost, p):
     p = logpost.to_params(p)
@@ -39,14 +39,15 @@ def plot_true_foreground_density(logpost, p):
     cm = logpost.covariance_matrix(p)
     mu = p['mu']
 
-    xmin = min(mu[0] - 5.0*cm[0,0], np.min(logpost.pts[:,0]))
-    xmax = max(mu[0] + 5.0*cm[0,0], np.max(logpost.pts[:,0]))
-    ymin = min(mu[1] - 5.0*cm[1,1], np.min(logpost.pts[:,1]))
-    ymax = max(mu[1] + 5.0*cm[1,1], np.max(logpost.pts[:,1]))
+    xmin = np.min(logpost.candidates['Period'])
+    xmax = np.max(logpost.candidates['Period'])
+    ymin = np.min(logpost.candidates['Radius'])
+    ymax = np.max(logpost.candidates['Radius'])
 
-    XS, YS = np.meshgrid(np.linspace(xmin, xmax, 250), np.linspace(ymin, ymax, 250))
-    ZS = logpost.foreground_density(p, np.column_stack((XS.flatten(), YS.flatten()))).reshape((250, 250))
+    XS, YS = np.meshgrid(np.exp(np.linspace(np.log(xmin), np.log(xmax), 250)),
+                         np.exp(np.linspace(np.log(ymin), np.log(ymax), 250)))
+    ZS = logpost.foreground_density(p, XS.flatten(), YS.flatten()).reshape((250, 250))
 
-    pp.pcolormesh(np.exp(XS), np.exp(YS), ZS)
+    pp.pcolormesh(XS, YS, ZS)
     pp.xscale('log')
     pp.yscale('log')
